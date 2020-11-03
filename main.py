29d3f74b2f6858c28e563e282cf11a5b39187fae
Jonas Høft <jonashoft@live.dk>
2020-11-03 16:50:48 +0100

    added relay support

--- main.py
+++ main.py
@@ -6,6 +6,8 @@ GPIO.setwarnings(False)
 GPIO.setmode(GPIO.BOARD)
 GPIO.setup(31, GPIO.OUT)
 GPIO.setup(37, GPIO.OUT)
+GPIO.setup(11, GPIO.OUT)
+GPIO.setup(13, GPIO.OUT)
 
 # Time between min and max = 4 seconds
 DIMM_VALUE = 50
@@ -17,6 +19,28 @@ def home():
     if request.method == 'GET':
         return render_template('index.html'), 200
 
+@app.route('/light_chain', methods=['GET', 'POST'])
+def light_chain():
+    state = request.args.get('state', 0, type=str)
+    print(state)
+    if state == 'On':
+        GPIO.output(13, 0)
+        return jsonify(result='Turned on')
+    elif state == 'Off':
+        GPIO.output(13, 1)
+        return jsonify(result='Turned off')
+
+@app.route('/desk_lamp', methods=['GET', 'POST'])
+def light_chain():
+    state = request.args.get('state', 0, type=str)
+    print(state)
+    if state == 'On':
+        GPIO.output(11, 0)
+        return jsonify(result='Turned on')
+    elif state == 'Off':
+        GPIO.output(11, 1)
+        return jsonify(result='Turned off')
+
 @app.route('/background_process', methods=['GET', 'POST'])
 def background_process():
     state = request.args.get('state', 0, type=str)
@@ -26,7 +50,7 @@ def background_process():
         return jsonify(result='Turned on')
     elif state == 'Off':
         turnOffLights()
-        return jsonify(result='Turned on')
+        return jsonify(result='Turned off')
 
 @app.route('/dimm', methods=['GET', 'POST'])
 def background_dimm():
@@ -65,6 +89,8 @@ def dimm(dimm_value):
         DIMM_VALUE = 100
 
 if __name__ == '__main__':
+    GPIO.output(11, 0)
+    GPIO.output(13, 0)
     turnOffLights(4)
     turnOnLights(2)
     app.run(debug=True, port=80, host='0.0.0.0')
