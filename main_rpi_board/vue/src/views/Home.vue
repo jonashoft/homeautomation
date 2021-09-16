df2301ef0f68d5f301dac75d7d62bda8e6506715
Jonas Höft <jonashoft@live.dk>
2021-09-16 23:39:07 +0200

    css changes

--- main_rpi_board/vue/src/views/Home.vue
+++ main_rpi_board/vue/src/views/Home.vue
@@ -1,5 +1,6 @@
 <template>
   <div class="home">
+    <div v-if="!isMobile()">
       <p>Loftslamper</p>
       <button type = "button" v-on:click="ikea_lights('On')">Tænd</button>
       <button type = "button" v-on:click="ikea_lights('Off')">Sluk</button><br>
@@ -14,11 +15,39 @@
       <p>Lys kæde</p>
       <button type = "button" v-on:click="relay('chain', 'On')">Tænd</button>
       <button type = "button" v-on:click="relay('chain', 'Off')">Sluk</button><br>
+    </div>
+    <div v-else>
+      <p style="font-size : 25px;">Loftslamper</p>
+      <button type = "button" class="buttonMobile" v-on:click="ikea_lights('On')">Tænd</button>
+      <button type = "button" class="buttonMobile" v-on:click="ikea_lights('Off')">Sluk</button><br>
+      <p style="font-size : 25px;">Dæmpning</p>
+      <button type = "button" class="buttonMobile" v-on:click="ikea_lights(25)">25</button>
+      <button type = "button" class="buttonMobile" v-on:click="ikea_lights(50)">50</button>
+      <button type = "button" class="buttonMobile" v-on:click="ikea_lights(75)">75</button>
+      <button type = "button" class="buttonMobile" v-on:click="ikea_lights(100)">100</button><br>
+      <!--<p>Skrivebords lampe</p>  - - - Temporarily disabled
+      <button type = "button" v-on:click="relay('desk', 'On')">Tænd</button>
+      <button type = "button" v-on:click="relay('desk', 'Off')">Sluk</button><br>-->
+      <p style="font-size : 25px;">Lys kæde</p>
+      <button type = "button" class="buttonMobile" v-on:click="relay('chain', 'On')">Tænd</button>
+      <button type = "button" class="buttonMobile" v-on:click="relay('chain', 'Off')">Sluk</button><br>
+    </div>   
   </div>
 </template>
 
-<script>
+<style scoped>
+.buttonMobile {
+  width: 100px;
+  height: 50px;
+  margin-right: 5px;
+  margin-left: 5px;
+  border-radius: 10px;
+  background: #17c9e3;
+  font-size : 20px;
+  }
+</style>
 
+<script>
 export default {
   name: 'Home',
   methods : {
@@ -43,6 +72,13 @@ export default {
         alert("Error: " + err);
       }
     },
+    isMobile() {
+      if(/Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent)) {
+        return true
+      } else {
+        return false
+      }
+    }
   }
 }
 </script>
